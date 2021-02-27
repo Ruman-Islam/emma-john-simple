8b90c9981bc1faa88c831a89503174f0070f6b8a
Ruman-Islam <rumanislam48@gmail.com>
2021-02-27 13:59:41 +0600

    added

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart = props.cart;
+    
+    const total = cart.reduce((total, product) => total + product.price, 0);
+
+    const tax = total / 10;
+
+    let shipping = 0;
+    if (total > 35) {
+        shipping = 0;
+    }
+    else if (total > 15) {
+        shipping = 4.99;
+    }
+    else if (total > 0) {
+        shipping = 12.99;
+    }
+
+    const grandTotal = total + shipping + tax;
+
+    const formatNumber = num => {
+        const precision = num.toFixed(2);
+        return Number(precision);
+    }
+
+    return (
+        <div>
+            <h1>Order Summary</h1>
+            <p>Items Ordered: {cart.length}</p>
+            <p>Product Price: {formatNumber(total)}</p>
+            <p><small>Shipping Cost: {formatNumber(shipping)}</small></p>
+            <p><small>Tax: {formatNumber(tax)}</small></p>
+            <p>Total Price: {formatNumber(grandTotal)}</p>
+        </div>
+    );
+};
+
+export default Cart;
